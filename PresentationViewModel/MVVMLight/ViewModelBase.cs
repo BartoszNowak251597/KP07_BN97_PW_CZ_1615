c86f43242fd19317d6d44f6711167a8362fa8a2e
BartoszNowak251597 <251597@edu.p.lodz.pl>
2025-03-23 20:15:08 +0100

    MVVM

---
+++ PresentationViewModel/MVVMLight/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace PresentationViewModel.MVVMLight
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion INotifyPropertyChanged
+
+        #region API
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion API
+    }
+}
